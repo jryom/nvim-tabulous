80cccf4103f6ebcc3707a002df4abff8162c4041
Jesper Ryom <2834949+jryom@users.noreply.github.com>
2023-10-06 12:54:47 +0200

    feat: add user configurable options

--- README.md
+++ README.md
@@ -3,3 +3,25 @@
 Clean tabline for Neovim. Shows windows per tab with a separator.
 
 <img width="1804" alt="Screenshot 2023-10-06 at 11 02 43" src="https://github.com/jryom/nvim-tabulous/assets/2834949/e79f56de-79b8-4871-b97c-d688d7a27021">
+
+## Installation
+
+With `lazy.nvim`:
+
+```lua
+{
+    'jryom/nvim-tabulous',
+    -- Default options
+    opts = {
+        show_index = true,
+        fnamemodify = ":t",
+        modified_string = " +",
+        max_windows = 3, -- Max windows shown per tab
+        auto_hide = true, -- Hide when only one tab
+        separator = "▕",
+        window_separator = " | ",
+        padding = "   ", -- Horizontal padding in each tab
+        ignored_buftypes = { "nofile" },
+    },
+}
+```
--- lua/tabline.lua
+++ lua/tabline.lua
@@ -1,10 +1,28 @@
 local M = {}
 
-local function predicate(buffer)
-	return vim.bo[buffer].buftype ~= "nofile"
+local function is_ignored(ignored_buftypes, buf_nr)
+	for _, buftype in ipairs(ignored_buftypes) do
+		if vim.bo[buf_nr].buftype == buftype then
+			return true
+		end
+	end
+
+	return false
 end
 
-local function tabline()
+M.options = {
+	show_index = true,
+	fnamemodify = ":t",
+	modified_string = " +",
+	max_windows = 3,
+	auto_hide = true,
+	separator = "▕",
+	window_separator = " | ",
+	padding = "   ",
+	ignored_buftypes = { "nofile" },
+}
+
+local function tabline(options)
 	local s = ""
 
 	for tab_nr = 1, vim.fn.tabpagenr("$") do
@@ -14,16 +32,19 @@ local function tabline()
 			s = s .. "%#TabLine#"
 		end
 
-		s = s .. "   "
-		s = s .. "%" .. tab_nr .. "T"
-		s = s .. tab_nr .. ". "
+		s = s .. options.padding
+
+		if options.show_index then
+			s = s .. "%" .. tab_nr .. "T"
+			s = s .. tab_nr .. ". "
+		end
 
 		local tabname = ""
 
 		local valid_buffers = {}
 
 		for _, buf_nr in ipairs(vim.fn.tabpagebuflist(tab_nr)) do
-			if predicate(buf_nr) then
+			if not is_ignored(options.ignored_buftypes, buf_nr) then
 				table.insert(valid_buffers, buf_nr)
 			end
 		end
@@ -32,39 +53,46 @@ local function tabline()
 			local buffername = vim.api.nvim_buf_get_name(buf_nr)
 			local buftype = vim.bo[buf_nr].buftype
 
+			if idx > options.max_windows then
+				break
+			end
+
 			if buftype == "help" then
-				buffername = vim.fn.fnamemodify(buffername, ":t:s/.txt$/ /") .. "[H]"
+				buffername = vim.fn.fnamemodify(buffername, options.fnamemodify .. ":s/.txt$/ /") .. "[H]"
 			elseif buftype == "quickfix" then
 				buffername = buffername .. "[Q]"
 			elseif buffername == "" then
 				buffername = "[No Name]"
 			else
-				buffername = vim.fn.fnamemodify(buffername, ":t")
+				buffername = vim.fn.fnamemodify(buffername, options.fnamemodify)
 			end
 
 			if vim.bo[buf_nr].modified then
-				buffername = buffername .. " +"
+				buffername = buffername .. options.modified_string
 			end
 
 			if idx < table.getn(valid_buffers) then
-				buffername = buffername .. " | "
+				buffername = buffername .. options.window_separator
 			end
 
 			tabname = tabname .. buffername
 		end
 
-		s = s .. tabname .. "  ▕"
+		s = s .. tabname .. string.sub(options.padding, 1, #options.padding - 1) .. options.separator
 	end
 
 	return s .. "%#TabLineFill#%T"
 end
 
-function M.setup()
+function M.setup(user_options)
+	M.options = vim.tbl_extend("force", M.options, user_options)
+
 	function _G.nvim_tabline()
-		return tabline()
+		return tabline(M.options)
 	end
 
-	vim.o.showtabline = 1
+	vim.o.showtabline = M.options.auto_hide and 1 or 2
+
 	vim.o.tabline = "%!v:lua.nvim_tabline()"
 
 	vim.g.loaded_nvim_tabulous = 1
